9bbf233f638a52190b85b48c10882805587fc24f
daifiyum <daiqisen131@gmail.com>
2024-08-29 10:38:59 +0800

    fix pm

--- revanced-magisk/service.sh
+++ revanced-magisk/service.sh
@@ -3,49 +3,59 @@ MODDIR=${0%/*}
 RVPATH=/data/adb/rvhc/${MODDIR##*/}.apk
 . "$MODDIR/config"
 
-until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
-until [ -d "/sdcard/Android" ]; do sleep 1; done
-while true; do
-	BASEPATH=$(pm path "$PKG_NAME" 2>/dev/null)
-	svcl=$?
-
-	if [ $svcl -eq 0 ]; then
-		break
-	elif [ $svcl -eq 20 ]; then
-		sleep 2
-	else
-		sleep 2
-	fi
-done
-sleep 5
-
 err() {
 	[ ! -f "$MODDIR/err" ] && cp "$MODDIR/module.prop" "$MODDIR/err"
 	sed -i "s/^des.*/description=⚠️ Needs reflash: '${1}'/g" "$MODDIR/module.prop"
 }
 
-if [ $svcl != 0 ]; then
+until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
+until [ -d "/sdcard/Android" ]; do sleep 1; done
+
+rm -f ${MODDIR}/packages.xml
+if [ ! -f "/data/system/packages.xml" ]; then
+	err "packages.xml not found"
+	exit
+elif [ -z "$(file /data/system/packages.xml | grep 'Binary')" ]; then
+	cp -f /data/system/packages.xml ${MODDIR}/packages.xml
+elif ! which abx2xml >/dev/null; then
+	err "abx2xml tool not found"
+	exit
+else
+	abx2xml /data/system/packages.xml ${MODDIR}/packages.xml
+fi
+
+PACKAGE_INFO=$(grep "<package name=\"${PKG_NAME}\"" ${MODDIR}/packages.xml)
+rm -f ${MODDIR}/packages.xml
+
+if [ -z "${PACKAGE_INFO}" ]; then
 	err "app not installed"
 	exit
 fi
-BASEPATH=${BASEPATH##*:} BASEPATH=${BASEPATH%/*}
+
+BASEPATH=$(echo ${PACKAGE_INFO} | awk -F 'codePath="' '{print $2}' | awk -F '"' '{print $1}')
+
 if [ ! -d "$BASEPATH/lib" ]; then
 	err "zygote crashed (fix your ROM)"
 	exit
 fi
+
 VERSION=$(dumpsys package "$PKG_NAME" | grep -m1 versionName) VERSION="${VERSION#*=}"
 if [ "$VERSION" != "$PKG_VER" ] && [ "$VERSION" ]; then
 	err "version mismatch (installed:${VERSION}, module:$PKG_VER)"
 	exit
 fi
+
 grep "$PKG_NAME" /proc/mounts | while read -r line; do
 	mp=${line#* } mp=${mp%% *}
 	umount -l "${mp%%\\*}"
 done
+
 if ! chcon u:object_r:apk_data_file:s0 "$RVPATH"; then
 	err "apk not found"
 	exit
 fi
+
 mount -o bind "$RVPATH" "$BASEPATH/base.apk"
 am force-stop "$PKG_NAME"
+
 [ -f "$MODDIR/err" ] && mv -f "$MODDIR/err" "$MODDIR/module.prop"
